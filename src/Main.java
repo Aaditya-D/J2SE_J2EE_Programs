cba6e49d79c95cf0c8ef29bd5fca1335c2d68ed5
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-03-09 23:12:33 +0530

    Added Program2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 /*
  * 
  * All programs will be executed from here.
+ * Uncomment Desired Class file before execution.
  * 
  */
 public class Main {
@@ -8,6 +9,8 @@ public class Main {
 	public static void main(String []a) {
 		Program1 p1 = new Program1();
 //		p1.buffer();
-		p1.scanner();
+//		p1.scanner();
+		Program2 p2 = new Program2();
+		p2.op();
 	}
 }
---
+++ src/Program2.java
@@ -0,0 +1,28 @@
+
+/*
+ * 
+ * Program to implement different data types.
+ * 
+ */
+
+public class Program2 {
+
+		void op() {
+		boolean b = true;
+		if(!b == false) {
+			System.out.println("^_^\n");
+			
+		char c= '.';
+		String s = "Hey !";
+		int i = 20;
+		byte e = 21;
+		short sh = 555;
+		float f = 4.4567851f;
+		double d = 4.542;
+		long l = 456451235;
+			System.out.println(c+"\t"+s+"\t"+i+"\t"+e+"\t"+sh+"\t"+f+"\t"+d+"\t"+l);
+		}
+	}
+	
+	
+}
