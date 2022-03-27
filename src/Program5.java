810242c130705a1ba344648961e6d71b5f66ead0
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-03-27 23:09:56 +0530

    Added Program5

--- src/Program5.java
+++ src/Program5.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 
 public class Program5 {
 
-	public Program5() {
-		
+	static void table() {
+		System.out.println("Enter the number for the multiplication Table: ");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+		for(int i=0;i<10;i++) {
+			System.out.println(a+" X "+ (i+1) +" ="+(a*(i+1)));
+		}
+	}
+	public static void main(String []a) {
+		table();
 	}
 
 }
