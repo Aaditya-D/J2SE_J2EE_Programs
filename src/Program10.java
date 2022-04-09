c6f3a5146d03ddb2ea2d3c02f2fd57c7840a9115
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-04-10 00:56:58 +0530

    Added Program10

---
+++ src/Program10.java
@@ -0,0 +1,23 @@
+
+public class Program10 {
+
+	int a;
+	Program10(){
+		this(5000);		//called parameterized constructor
+		System.out.println("Default called");
+	}
+	Program10(int a) {
+//		this();					//calls the default constructor
+		this.a=a;				//refers to instance variable 
+		System.out.println(this.a);
+		System.out.println(this);		//holds same address as the class's object
+	}
+
+	public static void main(String[] args) {
+		Program10 p10 = new Program10(2000);
+		System.out.println(p10);
+		Program10 p101 = new Program10();
+
+	}
+
+}
