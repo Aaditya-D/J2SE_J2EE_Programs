32096e365ef5db206f946295136cc74b660e51c4
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-04-30 22:03:21 +0530

    Added Program15

---
+++ src/program15.java
@@ -0,0 +1,42 @@
+abstract class Bike { // Abstract class
+	void wheels() { // Regular method
+		int wheels = 2;
+		System.out.println("wheels: " + wheels);
+	}
+
+	abstract void brake(); // Abstract method
+}
+
+class SportBike extends Bike {
+
+	@Override
+	void brake() {
+		String brake = "Disc brakes";
+		System.out.println("Bike: Sports Bike");
+		System.out.println("Brakes: " + brake);
+	}
+
+}
+
+class MountainBike extends Bike {
+
+	@Override
+	void brake() {
+		String brake = "Mountain Bike brakes";
+		System.out.println("Bike: Mountain Bike");
+		System.out.println("Brakes: " + brake);
+	}
+
+}
+
+public class program15 {
+	public static void main(String[] args) {
+		SportBike sb = new SportBike();
+		sb.wheels();
+		sb.brake();
+		MountainBike mb = new MountainBike();
+		mb.wheels();
+		mb.brake();
+
+	}
+}
