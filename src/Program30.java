8a57e23b5ef530b5a075ca9fa4c4d277988b7307
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-07-02 14:35:13 +0530

    Added Program30

--- src/Program30.java
+++ src/Program30.java
@@ -6,18 +6,26 @@ import java.sql.Statement;
 
 public class Program30 {
 
-	static String url = "";
+	static String url = "jdbc:mysql://localhost:3306/test_database";
 	static String user = "root";
 	static String password = "root";
 
 	public static void main(String[] args) {
 
 		String query = "select * from students";
-
+		/*
+		 * Not needed in the latest version of JDBC
+		try {//additional try-catch needed because of not AutoCloseable
+			Class.forName("com.mysql.cj.jdbc.Driver");		//loads the driver into memory
+		} catch (ClassNotFoundException e1) {
+			System.out.println("Exception occurred while loading driver:"+e1.getException());
+		}
+		*/
 		try (Connection con = DriverManager.getConnection(url, user, password);
-				Statement st = con.createStatement();) { // Connection created using try with multiple resources
+				Statement st = con.createStatement();) { // Connection and statement created using try with multiple resources
+			System.out.println("Connection Created....");
 			try {
-				ResultSet rs = st.executeQuery(query);// ResultSet handles the rows
+				ResultSet rs = st.executeQuery(query);// ResultSet stores the data fetched from table rows
 				while (rs.next()) {
 					System.out.println("Name:" + rs.getString("name"));
 					System.out.println("Age:" + rs.getString("age"));
@@ -26,7 +34,7 @@ public class Program30 {
 				e.getMessage();
 			}
 		} catch (Exception e) {
-			e.getMessage();
+			e.printStackTrace();
 		}
 	}
 }
