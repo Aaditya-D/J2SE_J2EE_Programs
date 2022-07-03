21e7dee9ed968672e3f0a8cb051b182e48bab62a
Aaditya Dwivedi <aadityadwivedi128@gmail.com>
2022-07-03 14:36:32 +0530

    Added Program34

--- src/Program34.java
+++ src/Program34.java
@@ -1,29 +1,40 @@
-class C1 extends Thread{
-		@Override
-		public void run() {	//Thread execution starts from run method
-			for (int i=0;i<5;++i) {
-				System.out.println("Thread 1 Running......");
-				try {Thread.sleep(1000);}catch (Exception e) {}
-			}
-		}
-}
-class C2 extends Thread{
-	
+class C1 extends Thread {
 	@Override
-	public void run() {
-		for (int i=0;i<5;++i) {
-			System.out.println("Thread 2 Running......");
-			try {Thread.sleep(1000);}catch (Exception e) {}
+	public void run() { // Thread execution starts from here
+		for (int i = 0; i < 5; ++i) {
+			System.out.println("Thread 1 Running......");
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
 		}
 	}
 }
-class C3 extends Thread{
-	
+
+class C2 extends Thread {
+
 	@Override
 	public void run() {
-		for (int i=0;i<5;++i) {
+		for (int i = 0; i < 5; ++i) {
+			System.out.println("Thread 2 Running......");
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
+		}
+	}
+}
+
+class C3 extends Thread {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 5; ++i) {
 			System.out.println("Thread 3 Running......");
-			try {Thread.sleep(1000);}catch (Exception e) {}
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+			}
 		}
 	}
 }
@@ -31,17 +42,17 @@ class C3 extends Thread{
 public class Program34 {
 
 	public static void main(String[] args) {
-	
+
 		C1 c1 = new C1();
 		C2 c2 = new C2();
 		C3 c3 = new C3();
-		
+
 		c1.setName("Thread 1");
-		c1.start();	//Calls the run method of Thread
+		c1.start(); // Calls the run method of Thread
 		c2.setName("Thread 2");
 		c2.start();
 		c3.setName("Thread 3");
 		c3.start();
-		
+
 	}
 }
